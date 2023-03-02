f47e8977195c4ff934162483397a5f13610948a9
Vu Xuan Truong <101324265+davitvu@users.noreply.github.com>
2023-03-02 12:53:51 +0700

    comment clock js

--- js.js
+++ js.js
@@ -56,24 +56,24 @@ close_btn_register_overlay = () => $('.register-modal_none').style.display = "no
 close_btn_login_overlay = () => $('.login-modal_none').style.display = "none";
 close_btn_login = () => $('.login-modal_none').style.display = "none";
 
-// clock
-function updateTimer() {
-        var now = new Date();
-        var end = new Date("July 1, 2023");
+// // clock
+// function updateTimer() {
+//         var now = new Date();
+//         var end = new Date("July 1, 2023");
 
-        var timeDiff = end.getTime() - now.getTime();
+//         var timeDiff = end.getTime() - now.getTime();
 
-        var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+//         var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+//         var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//         var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+//         var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-        var totalHours = days * 24 + hours;
-        var totalMinutes = totalHours * 60 + minutes;
+//         var totalHours = days * 24 + hours;
+//         var totalMinutes = totalHours * 60 + minutes;
 
-        document.getElementById("timer").innerHTML = days + " ngày " + hours + " giờ " + minutes + " phút " + seconds + " giây ";
-        document.getElementById("total-hours").innerHTML = totalHours + " giờ " + minutes + " phút";
-        document.getElementById("total-minutes").innerHTML = totalMinutes + " phút " + seconds + " giây";
-      }
+//         document.getElementById("timer").innerHTML = days + " ngày " + hours + " giờ " + minutes + " phút " + seconds + " giây ";
+//         document.getElementById("total-hours").innerHTML = totalHours + " giờ " + minutes + " phút";
+//         document.getElementById("total-minutes").innerHTML = totalMinutes + " phút " + seconds + " giây";
+//       }
 
-      setInterval(updateTimer, 1000);
+//       setInterval(updateTimer, 1000);
